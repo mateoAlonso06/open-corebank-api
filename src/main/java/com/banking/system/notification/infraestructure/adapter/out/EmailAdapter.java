abfc7b2d6c8e28c86b2017bea6be41192eb97c0e
mateoAlonso06 <alonsomateo2002@gmail.com>
2026-03-04 19:40:59 -0300

    refactor: adds some new properties to the mail config

--- src/main/java/com/banking/system/notification/infraestructure/adapter/out/EmailAdapter.java
+++ src/main/java/com/banking/system/notification/infraestructure/adapter/out/EmailAdapter.java
@@ -5,9 +5,11 @@ import com.banking.system.notification.domain.model.EmailNotification;
 import com.banking.system.notification.domain.port.out.EmailSenderPort;
 import com.banking.system.notification.domain.port.out.TemplateRenderedPort;
 import jakarta.mail.MessagingException;
+import java.io.UnsupportedEncodingException;
 import jakarta.mail.internet.MimeMessage;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.mail.javamail.MimeMessageHelper;
 import org.springframework.stereotype.Component;
@@ -19,12 +21,19 @@ public class EmailAdapter implements EmailSenderPort {
     private final JavaMailSender javaMailSender;
     private final TemplateRenderedPort templateRenderedPort;
 
+    @Value("${spring.mail.from-address:no-reply@localhost}")
+    private String fromAddress;
+
+    @Value("${spring.mail.from-name:Core Bank}")
+    private String fromName;
+
     @Override
     public void sendEmail(EmailNotification notification) {
         try {
             MimeMessage message = javaMailSender.createMimeMessage();
             MimeMessageHelper helper = new MimeMessageHelper(message, true, "UTF-8");
 
+            helper.setFrom(fromAddress, fromName);
             helper.setTo(notification.to());
             helper.setSubject(notification.subject());
 
@@ -37,7 +46,7 @@ public class EmailAdapter implements EmailSenderPort {
             javaMailSender.send(message);
 
             log.info("Email sent successfully to {}", notification.to());
-        } catch (MessagingException e) {
+        } catch (MessagingException | UnsupportedEncodingException e) {
             log.error("Error sending email to: {}", notification.to(), e);
             throw new EmailDeliveryException("Failed to send email to " + notification.to(), e);
         }
